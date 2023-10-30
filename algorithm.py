5163a3ff0f31e479ef9b5cb3604881d34d24307b
Flychee <1229853676@qq.com>
2023-10-31 04:05:12 +0800

    考虑手肘法内部实现及意义，将手肘法迁移至visualization.py

--- algorithm.py
+++ algorithm.py
@@ -64,19 +64,3 @@ class KM:
             for temp, point in zip(temp_matrix, self.point_matrix):
                 balance_bool = True if temp.cluster == point.cluseter else False
             num += 1
-
-
-# 手肘法
-def SSE(filename):
-    result = []
-    for center_num in range(2, 10):
-        min_loss = 10000
-        for i in range(50):
-            km = KM(filename, center_num)
-            km.init_center()
-            km.iter()
-            loss = sum([i.min_distance for i in km.point_matrix]) / len(km.point_matrix)
-            if loss < min_loss:
-                min_loss = loss
-        result.append(min_loss)
-    print(result)
