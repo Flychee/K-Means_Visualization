9608a6bfb5d3aaaee13dca09e28e9f02cd6dd907
Flychee <1229853676@qq.com>
2023-11-05 19:51:32 +0800

    最后的bug，游戏结束

--- algorithm.py
+++ algorithm.py
@@ -3,7 +3,6 @@ import copy
 import random
 
 
-# 迭代点类
 class Point:
     def __init__(self, position):
         self.position = position
@@ -13,7 +12,7 @@ class Point:
     def choose_cluster(self, center_list):
         for index, center in enumerate(center_list):
             temp_distance = np.linalg.norm(self.position - center)
-            if temp_distance < self.min_distance:
+            if temp_distance <= self.min_distance:
                 self.min_distance = temp_distance
                 self.cluster = index
 
@@ -21,13 +20,21 @@ class Point:
 # 算法类
 class KM:
     def __init__(self, filename, center_num):
-        self.point_matrix = [Point(element) for element in np.load(filename)]  # 迭代点矩阵
+        read_npy = np.load(filename).astype(np.float64)
+        self.point_matrix = [Point(element) for element in read_npy]  # 迭代点矩阵
         self.center_list = []  # 中心点
         self.center_num = center_num  # 中心点个数
         self.iter_limit = 20  # 迭代次数上限
 
     #  选取初始中心点
     def init_center(self):
+        # 重置中心点列表
+        self.center_list = []
+        # 重置类归属
+        pm_len = len(self.point_matrix)
+        for index in range(pm_len):
+            self.point_matrix[index].cluster = None
+            self.point_matrix[index].min_distance = np.inf
         temp_matrix = copy.deepcopy(self.point_matrix)
         for _ in range(self.center_num):
             temp = random.choice(temp_matrix)
@@ -36,10 +43,16 @@ class KM:
 
     #  读取坐标
     def read_position(self):
-        result = []
+        none_result = []
+        cluster_result = [copy.deepcopy([]) for _ in self.center_list]
         for point in self.point_matrix:
-            result.append(point.position)
-        return result
+            if point.cluster is None:
+                none_result.append(point.position)
+            else:
+                cluster_result[point.cluster].append(point.position)
+        for item in range(len(cluster_result)):
+            cluster_result[item] = np.array(cluster_result[item])
+        return np.array(none_result), cluster_result
 
     def read_cluster(self):
         result = []
@@ -76,3 +89,15 @@ class KM:
             for temp, point in zip(temp_matrix, self.point_matrix):
                 balance_bool = True if temp.cluster == point.cluster else False
             num += 1
+
+
+
+read_file = r'G:\LEARNING\K-Means_Visualization\Agents-200\001log_uniform_200.npy'
+
+km = KM(read_file, 10)
+km.init_center()
+km.iter()
+print(km.read_cluster())
+km.init_center()
+km.iter()
+print(km.read_cluster())
